c215f767096451d65a3d7c99a870c6a2ea696800
ramin azizi <ray@hititup.com>
2016-06-22 16:33:37 -0700

    built the JS Logic

---
+++ main.js
@@ -0,0 +1,9 @@
+var wdi_24_names = ["Adrianna", "Andrew", "Christian",
+"Daniel", "David", "Desi", "Emily", "Jenny", "Kate",
+"Kevin", "Matt", "Michael", "Ramin", "Taylor"];
+
+
+var i= Math.floor((Math.random()* wdi_24_names.length)+0);
+function eachname(){
+console.log(wdi_24_names[i]);
+}

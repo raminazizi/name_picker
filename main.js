fcfbd2d4c4712194407f6ac38233bcd41198d851
ramin azizi <ray@hititup.com>
2016-06-22 17:30:55 -0700

    added the bonus for picking only once

--- main.js
+++ main.js
@@ -7,4 +7,15 @@ var wdi_24_names = ["Adrianna", "Andrew", "Christian",
 function eachname(){
   var i= Math.floor((Math.random()* wdi_24_names.length));
 console.log(wdi_24_names[i]);
+
+// take out the used index
+wdi_24_names.splice(i,1);
+
+// After you take all out
+if (wdi_24_names.length == 0){
+  console.log("Called everyone once")
+  wdi_24_names.push("Adrianna", "Andrew", "Christian",
+  "Daniel", "David", "Desi", "Emily", "Jenny", "Kate",
+  "Kevin", "Matt", "Michael", "Ramin", "Taylor");
+}
 }
